2fa2224920954d4e4120b48bb4889f213d13be75
kuchu0102 <61591442+kuchu0102@users.noreply.github.com>
2020-07-22 13:21:51 +0100

    Add files via upload

---
+++ assgn_1_part_1_190649351/1_one_variable/calculate_hypothesis.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def calculate_hypothesis(X, theta, i):
+    """
+        :param X            : 2D array of our dataset
+        :param theta        : 1D array of the trainable parameters
+        :param i            : scalar, index of current training sample's row
+    """
+    
+    hypothesis = 0.0
+    #########################################
+    
+    hypothesis = X[i,0] * theta[0] + X[i,1] * theta[1]
+ 
+    #########################################
+    
+    return hypothesis
---
+++ assgn_1_part_2_190649351/1_logistic_regression/calculate_hypothesis.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sigmoid import *
+
+def calculate_hypothesis(X, theta, i):
+    """
+        :param X            : 2D array of our dataset
+        :param theta        : 1D array of the trainable parameters
+        :param i            : scalar, index of current training sample's row
+    """
+    hypothesis = 0.0
+    #########################################
+    # Write your code here
+    # You must calculate the hypothesis for the i-th sample of X, given X, theta and i.
+    a = X.shape[1]
+    for cnt in range(a):
+        hypothesis = hypothesis + X[i,cnt] * theta[cnt]
+    ########################################/
+    result = sigmoid(hypothesis)
+    
+    return result
